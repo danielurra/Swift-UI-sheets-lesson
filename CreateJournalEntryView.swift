5a6c74865611dbad832c86526461627fd96fdeca
Daniel Urra <danielurra75@gmail.com>
2024-03-27 05:13:19 -0500

    Add files via upload

---
+++ CreateJournalEntryView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateJournalEntryView.swift
+//  JournalEntries
+//
+//  Created by Eusebio Taba on 3/27/24.
+//
+
+import SwiftUI
+
+struct CreateJournalEntryView: View {
+    var body: some View {
+        Text("CREATE JOURNAL ENTRY")
+    }
+}
+
+#Preview {
+    CreateJournalEntryView()
+}
