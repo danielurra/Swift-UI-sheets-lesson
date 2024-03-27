9c8b532f34e97448ad2041011f602f9bbc1ffe85
Daniel Urra <danielurra75@gmail.com>
2024-03-27 05:17:24 -0500

    Add files via upload

---
+++ JournalEntriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalEntriesApp.swift
+//  JournalEntries
+//
+//  Created by Eusebio Taba on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct JournalEntriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            JournalEntriesListView()
+        }
+    }
+}
